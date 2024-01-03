c20fa03e6134eb6612040c4ac2a0c3341828ad80
Chris Hartgerink <chris@libscie.org>
2024-01-03 15:24:49 +0100

    Add code for seeding cash on hand history

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,61 +1,28 @@
-import type { Prisma } from '@prisma/client'
+import { Prisma, LogType } from '@prisma/client'
 import { db } from 'api/src/lib/db'
+
+// const dataRaw = [
+//   {
+//     date: 'YYYY-MM-DD',
+//     total: 123, // Not required
+//     cumulative: 123,
+//   },
+// ]
 
 export default async () => {
   try {
-    //
-    // Manually seed via `yarn rw prisma db seed`
-    // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
-    ]
-    console.log(
-      "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
-
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    await Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
-      })
-    )
-
-    // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
-    // and associated `salt` to their record. Here's how to create them using
-    // the same algorithm that dbAuth uses internally:
-    //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
-    //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
-    //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
+    // const processed = dataRaw.map((data, index) => {
+    //   const date = new Date(data.date)
+    //   date.setMilliseconds(date.getMilliseconds() + index)
+    //   // console.log(date.toISOString())
+    //   return {
+    //     time: date.toISOString(),
+    //     type: LogType.CashOnHand,
+    //     value: data.cumulative,
     //   }
+    // })
+    // await db.log.createMany({ data: processed })
+    // console.log(processed)
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
