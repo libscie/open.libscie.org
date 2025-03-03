cf64d3fb84915c4b1d9c86c9a54b597970496d00
Chris Hartgerink <chris@libscie.org>
2025-03-03 22:10:39 +0100

    Update config

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,7 +1,7 @@
 // See https://observablehq.com/framework/config for documentation.
 export default {
   // The project’s title; used in the sidebar and webpage titles.
-  title: "open.libscie.org",
+  title: "Liberate Science GmbH",
 
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
@@ -20,7 +20,7 @@ export default {
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
 
   // Some additional configuration options and their defaults:
-  theme: "deep-space", // try "light", "dark", "slate", etc.
+  theme: "light", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // toc: true, // whether to show the table of contents
